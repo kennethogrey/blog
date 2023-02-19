5886baa1c725ac7f087458d8629139b6f3b12bf5
kennethogrey <ogirekenneth@gmail.com>
2023-02-19 16:32:02 +0300

    update=>elfinder

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -8,5 +8,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 
-    config.filebrowserBrowserUrl = '/elfinder/ckeditor.php';
+    config.filebrowserBrowseUrl = '/elfinder/ckeditor';
 };
